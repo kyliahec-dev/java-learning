dcd96fecc0f1d0585d4c5e2002ba3ee330618ee3
Kyliah Carter <kcarte24@students.towson.edu>
2026-06-27 13:57:55 -0400

    Exercise 1 - Basics

---
+++ src/AboutMe.java
@@ -0,0 +1,15 @@
+public class AboutMe {
+    public static void main (String[] args) {
+        //Personal Info
+        String name = "Kyliah";
+        //Education
+        String degree = "BS in IT";
+        //Career
+        String job = "IT Analyst";
+
+        System.out.println("My name is " + name + " and I am an " + job + " with a " + degree + ".\n");
+        System.out.println("""
+        I am learning Java and trying to become proficient so that I can become a Software Engineer.\n
+        An interesting fact about me: I have worked in the Healthcare IT industry for about 5 years.""");
+    }
+}

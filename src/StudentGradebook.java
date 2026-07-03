259bf0238b04e227f5fccd268474966d2764ca58
Kyliah Carter <kcarte24@students.towson.edu>
2026-07-03 15:31:54 -0400

    Exercise 1 - Basics (Arrays) Updated Logic

--- src/StudentGradebook.java
+++ src/StudentGradebook.java
@@ -1,26 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class StudentGradebook {
     public static void main(String[] args) {
-        //Create object for user input
+        //Create object for user count
         Scanner scanner = new Scanner(System.in);
 
         //Create container for grades
         int[] grades = new int[5];
 
-        //User input
-        int input = 0; //counter
+        //User count
+        int count = 0; //counter
+        int newGrade = 0;
 
-        //Variables used to store and calculate sum and average
+        //Variable used to store and calculate sum and average
         int total = 0;
 
-        //Loop to collect user input for 5 grades
-        while (input < 5) {
-            System.out.print("Enter grade number " + (input + 1) + ": ");
-            grades[input] = scanner.nextInt();
-            input++; //increment input to stop prompting the user after receiving 5 grades
-            total += input;
-        }
+        //Loop to collect 5 grades
+        while (count < 5) {
+            System.out.print("Enter grade number " + (count + 1) + ": ");
+            newGrade = scanner.nextInt();
+            grades[count] = newGrade;
+            total += newGrade;
+            System.out.println("Total: " + total);
+            count++; //increment count to stop prompting the user after receiving 5 grades
+    }
+        System.out.println("Grades: " + Arrays.toString(grades));
         int avg = (total / grades.length);
         System.out.print("Average of grades is: " + avg);
     }
